167576d18d943b03676cd70b9ec402fd7e5a9be9
Wannes Salomé <11842931+waspeer@users.noreply.github.com>
2023-12-05 21:35:03 +0100

    add local @sanity-typed/types to sanity-studio, trigger 'no export named ...' error

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Reproduction for https://github.com/saiichihashimoto/sanity-typed/issues/394
 - I used a [NPM Workspace](https://docs.npmjs.com/cli/v7/using-npm/workspaces) to be able to use the local dependencies of @sanity-typed/types. I just included the folders `tsconfig`, `types` and `utils` to keep it minimal.
 - I built the `types` package by running `npm run build` inside the `types` folder. I used the same `tsup.config.ts` that's in the original repository.
 - I created a sanity-studio package with `pnpm create sanity@latest` and installed dependencies with `pnpm`.
+- I installed the local `types` package running `pnpm add ../sanity-typed/types` inside the `sanity-studio` folder and added [the schema provided in the readme](https://github.com/saiichihashimoto/sanity-typed/tree/main/packages/types#usage).
+- This results in the following error: `Uncaught error: The requested module '/@fs/Users/wannes/Projecten/Web/waspeer/sanity-typed-issue/sanity-typed/types/dist/index.js?t=1701806909825' does not provide an export named 'defineArrayMember'`
--- sanity-studio/sanity.config.ts
+++ sanity-studio/sanity.config.ts
@@ -1,15 +1,53 @@
-import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemas'
 
-export default defineConfig({
+// import {defineConfig} from 'sanity'
+import {defineConfig} from '@sanity-typed/types'
+import type {InferSchemaValues} from '@sanity-typed/types'
+
+// import {defineArrayMember, defineField, defineType} from 'sanity'
+import {defineArrayMember, defineField, defineType} from '@sanity-typed/types'
+
+const product = defineType({
+  name: 'product',
+  type: 'document',
+  title: 'Product',
+  fields: [
+    defineField({
+      name: 'productName',
+      type: 'string',
+      title: 'Product name',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'tags',
+      type: 'array',
+      title: 'Tags for item',
+      of: [
+        defineArrayMember({
+          type: 'object',
+          name: 'tag',
+          fields: [
+            defineField({type: 'string', name: 'label'}),
+            defineField({type: 'string', name: 'value'}),
+          ],
+        }),
+      ],
+    }),
+  ],
+})
+
+/** No changes using defineConfig */
+const config = defineConfig({
   projectId: '59t1ed5o',
   dataset: 'production',
-
   plugins: [deskTool(), visionTool()],
-
   schema: {
-    types: schemaTypes,
+    types: [product],
   },
 })
+
+export default config
+
+/** Typescript type of all types! */
+export type SanityValues = InferSchemaValues<typeof config>
